47c2a4843ae2f57b41c18d5d5b133acb6c9d69de
Robert Samardžija <samardzija.robert@gmail.com>
2022-12-11 14:44:45 +0100

    Day 8 part 2

--- solutions/day08.py
+++ solutions/day08.py
@@ -4,17 +4,32 @@ with open("../data/day08.txt", "r") as f:
     height_map = np.array([list(map(int, l.strip())) for l in f.readlines()])
 
     visible = 2 * (height_map.shape[0] + height_map.shape[1]) - 4
+    scenic = 0
 
     for y in range(1, height_map.shape[0] - 1):
         for x in range(1, height_map.shape[1] - 1):
             h = height_map[y, x]
 
-            if (
-                np.count_nonzero(height_map[y, :x] >= h) == 0
-                or np.count_nonzero(height_map[y, x + 1 :] >= h) == 0
-                or np.count_nonzero(height_map[:y, x] >= h) == 0
-                or np.count_nonzero(height_map[y + 1 :, x] >= h) == 0
-            ):
+            l = np.count_nonzero(height_map[y, :x] >= h)
+            r = np.count_nonzero(height_map[y, x + 1 :] >= h)
+            u = np.count_nonzero(height_map[:y, x] >= h)
+            d = np.count_nonzero(height_map[y + 1 :, x] >= h)
+
+            li = np.argwhere(height_map[y, :x] >= h)
+            li = x - li[-1][0] if li.size > 0 else x
+
+            ri = np.argwhere(height_map[y, x + 1 :] >= h)
+            ri = ri[0][0] + 1 if ri.size > 0 else height_map.shape[1] - x - 1
+
+            ui = np.argwhere(height_map[:y, x] >= h)
+            ui = y - ui[-1][0] if ui.size > 0 else y
+
+            di = np.argwhere(height_map[y + 1 :, x] >= h)
+            di = di[0][0] + 1 if di.size > 0 else height_map.shape[0] - y - 1
+
+            if l == 0 or r == 0 or u == 0 or d == 0:
                 visible += 1
 
-    print(visible)
+            scenic = li * ri * ui * di if li * ri * ui * di > scenic else scenic
+
+    print(visible, scenic)
